4cb9951194cb6e4525d249e52c28cd5cad0218ca
Jelly <jeremygohct@gmail.com>
2023-08-13 11:56:54 +0800

    c08

---
+++ c08/ex05/ft_show_tab.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+	
+	nbr = nb;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+		ft_putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + '0');
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	while (par->str)
+	{
+		ft_putstr(par->str);
+		ft_putchar('\n');
+		ft_putnbr(par->size);
+		ft_putchar('\n');
+		ft_putstr(par->copy);
+		ft_putchar('\n');
+		par++;
+	}
+}
