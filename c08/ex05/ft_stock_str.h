b4437f9f38a84b5e3e0ec5a31f41aacf3ce111a8
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-08-16 14:18:49 +0800

    Update ft_stock_str.h

--- c08/ex05/ft_stock_str.h
+++ c08/ex05/ft_stock_str.h
@@ -1,7 +1,19 @@
-#ifndef FT_STOCK_STR_H
-#define FT_STOCK_STR_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergoh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 13:50:26 by jergoh            #+#    #+#             */
+/*   Updated: 2023/08/16 13:51:39 by jergoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct	s_stock_str
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
 {
 	int		size;
 	char	*str;
