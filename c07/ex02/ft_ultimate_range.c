02c56cdaec7902fba81fc38d5da32695935a9edb
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-08-16 17:27:01 +0800

    Update ft_ultimate_range.c

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -25,3 +25,22 @@ int	ft_ultimate_range(int **range, int min, int max)
 		return (-1);
 	return (max - min);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	*result;
+	int	min = 5;
+	int	max = 12;
+	
+	printf("Min: %d, Max: %d\n", min, max);
+	int	size = ft_ultimate_range(&result, min, max);
+	printf("Generated array: ");
+	for (int i = 0; i < size; i++)
+		printf("%d ", result[i]);
+	printf("\n");
+	free(result);
+	return 0;
+}
+*/
