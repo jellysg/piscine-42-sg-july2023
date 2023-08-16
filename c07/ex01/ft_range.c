2d6f98094e4112eb34b0ef4e98efaddffafd9144
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-08-16 17:13:43 +0800

    Update ft_range.c

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -14,3 +14,26 @@ int	*ft_range(int min, int max)
 	else
 		return (0);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int	min = -10;
+	int	max = 10;
+	int *result = ft_range(min, max);
+	if (result == NULL) {
+		printf("Invalid input range\n");
+	return 1;
+	}
+	printf("Min: %d,\nMax: %d,\n", min, max);
+	printf("Generated array: ");
+	for (int i = 0; i < max - min; i++)
+		printf("%d ", result[i]);
+	printf("\n");
+
+    free(result);
+
+    return 0;
+}
+*/
