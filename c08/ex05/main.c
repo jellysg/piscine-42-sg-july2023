d2ddaedcaba9630b14dca1585c074356e0141f1b
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-08-16 14:15:39 +0800

    Update main.c

--- c08/ex05/main.c
+++ c08/ex05/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ft_stock_str.h"
 
 t_stock_str	*ft_strs_to_tab(int ac, char **av);
@@ -6,11 +7,12 @@ void	ft_show_tab(struct s_stock_str *par);
 
 int main(int argc, char **argv)
 {
-  t_stock_str *tab;
-	tab = ft_strs_to_tab(argc, argv);
+	t_stock_str *tab;
+	tab = ft_strs_to_tab(argc - 1, &argv[1]);
 	ft_show_tab(tab);
+
 	for (int i = 0; i < argc; i++)
-    free(tab[i].copy);
+		free(tab[i].copy);
 	free(tab);
 	return (0);
 }
