4151e6c3bd904e4038d0458e10afb070a8262e0d
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-08-17 08:44:02 +0800

    Update ft_is_sort.c

--- c11/ex04/ft_is_sort.c
+++ c11/ex04/ft_is_sort.c
@@ -1,27 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergoh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 19:14:39 by jergoh            #+#    #+#             */
+/*   Updated: 2023/08/16 19:14:40 by jergoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
-	int	count;
 	int	i;
+	int	flag;
 
 	i = 0;
-	count = 0;
-	while (i++ < length - 1)
+	flag = 0;
+	while (i < length - 1)
 	{
-		if (f(*tab, *(tab + 1)) <= 0)
-			count++;
-		tab++;
+		if (flag == 0 && f(tab[i], tab [i + 1]) != 0)
+			flag = (f(tab[i], tab[i + 1]));
+		else if (flag > 0 && (f(tab[i], tab[i + 1])) < 0)
+			return (0);
+		else if (flag < 0 && (f(tab[i], tab[i + 1])) > 0)
+			return (0);
+		i++;
 	}
-	return (count == length - 1);
+	return (1);
 }
-/*
+
 #include <stdio.h>
 
-int compare(int a, int b) {
-    if (a < b) return -1;
-    if (a == b) return 0;
-    return 1;
+int compare(int a, int b)
+{
+	if (a && b)
+		return (a - b);
+	else
+		return (1);
 }
-
+/*
 int	main(void)
 {
 	int	array1[] = {1, 2, 3, 4, 5};
@@ -31,7 +49,7 @@ int	main(void)
 		printf("%d, ", array1[i]);
 	printf("\n\n");
 
-	int array2[] = {5, 3, 4, 2, 1};
+	int array2[] = {5, 4, 3, 2, 1};
 	int length2 = sizeof(array2) / sizeof(array2[0]);
 	printf("Array B: Returns %d\n", ft_is_sort(array2, length2, compare));
 	for(int i = 0; i < length1 ; i++)
