d3841984b8ea1bde4ab9b5ab4f7e3d5a0ebbe544
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-08-17 09:16:48 +0800

    Update and rename operators.c to calc.c

---
+++ c11/ex05/calc.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calc.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergoh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 19:27:47 by jergoh            #+#    #+#             */
+/*   Updated: 2023/08/16 19:27:49 by jergoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+
+int	add(int a, int b)
+{
+	return (a + b);
+}
+
+int	subtract(int a, int b)
+{
+	return (a - b);
+}
+
+int	multiply(int a, int b)
+{
+	return (a * b);
+}
+
+int	divide(int a, int b)
+{
+	return (a / b);
+}
+
+int	modulo(int a, int b)
+{
+	return (a % b);
+}
